f04b1ded4918281ef4eed026f1ad6a5c5df8bcda
Elena <elenasm753@gmail.com>
2024-10-22 23:08:59 +0300

    Добавление стрельбы и анимаций

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,11 +14,16 @@ public class PlayerController : MonoBehaviour
     public float moveInput;
     public float rayDistance = 0.6f;
     float horizantalMove = 0f;
-    public Animator anim;
 
+    
+    public Animator anim; //Анимация 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
 
+    public GameObject bullet; // Патрон
+    public Transform shotPoint; // Место откуда будет лететь шишка
+    private float timeBtwShots; //КД дброска
+    public float startTimeBtwShots; //время перезорядки
 
 
     private void Start() {
@@ -31,8 +36,10 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate() {
         moveInput = Input.GetAxis("Horizontal"); // эт для того чтобы ходил онли по горизонтали
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y); // переменные x и y
+                                                                     // Получить скорость тока (Vector2)
+     
 
-        if(facingRight == false && moveInput > 0) // переварот персонажа в стонону A || D
+        if (facingRight == false && moveInput > 0) // переварот персонажа в стонону A || D
         {
             Flipe();
         }
@@ -44,8 +51,10 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && (jump < 2))
+        
+        if (Input.GetKeyDown(KeyCode.Space) && jump < 2)
         {
+            rb.velocity = new Vector2(rb.velocity.x, 0); // Обнуляем вертикальную скорость перед прыжком
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             jump += 1;
         }
@@ -53,8 +62,18 @@ public class PlayerController : MonoBehaviour
         horizantalMove = Input.GetAxisRaw("Horizontal") * speed;
         anim.SetFloat("Speed", Mathf.Abs(horizantalMove)); //Передача информации в Animator в значении Speed
         anim.SetFloat("Jump", jump); //Передача информации в Animator в значении количество прыжков Jump
-        anim.SetFloat("Ground", isGround); //Передача информации в Animator в значении количество прыжков Jump
 
+        if (timeBtwShots <= 0 ) { //Для того чтобы не спамить атаками
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Instantiate(bullet, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots; //рестарт перезарядки
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
 
     }
     private void OnCollisionEnter2D(Collision2D collision) // В данном способе мы будем использовать метод OnCollisionEnter2D() – который срабатывает тогда, когда наш объект соприкасается с другим объектом
